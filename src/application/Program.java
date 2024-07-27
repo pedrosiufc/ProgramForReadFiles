fd2774f741168554479e6e0a7c54ab6184a60614
pedrosiufc <pedrosiufc@gmail.com>
2024-07-27 14:42:30 -0300

    Second example code

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,54 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
-	
 	public static void main(String[] args) {
-		File file = new File("C:\\Users\\Pedro\\Git\\Eclipse_Workspace\\ProgramForReadFiles\\temp\\in.txt");
-		Scanner sc = null;
+
+		/*
+		 * File file = new File(
+		 * "C:\\Users\\Pedro\\Git\\Eclipse_Workspace\\ProgramForReadFiles\\temp\\in.txt"
+		 * ); Scanner sc = null; try { sc = new Scanner(file); while (sc.hasNextLine())
+		 * { System.out.println(sc.nextLine()); } } catch (IOException e) {
+		 * System.out.println("Error: " + e.getMessage()); } finally { if (sc != null) {
+		 * sc.close(); } } }
+		 */
+
+		//Second example code
+		
+		String path = "C:\\Users\\Pedro\\Git\\Eclipse_Workspace\\ProgramForReadFiles\\temp\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
+
 		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+
+			String line = br.readLine();
+
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			if (sc != null) {
-				sc.close();
+		}
+
+		finally {
+			try {
+
+				if (br != null) {
+					br.close();
+				}
+				if (fr != null) {
+					fr.close();
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
